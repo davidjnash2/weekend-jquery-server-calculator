c63bf634f28070c2928336a0de121b6498f2da25
Dave Nash <davidjnash2@gmail.com>
2023-05-12 22:33:02 -0500

    removed reminder note from html, on serverjs added empty results array, started build of math function, added appget functions for math and results, added post refunction for adding data to server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,46 @@ const PORT = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
+let results = [];
+
+//1. { numOne: x, numTwo: x }
+// function mathAboutIt(inputs) {
+//  let result = 0;
+//     if (inputs.operator === '+'){
+//       result = Number(inputs.num1) + Number(inputs.num2);
+//     } else if (inputs.operator === '-'){
+//       result = Number(inputs.num1) - Number(inputs.num2);          
+//     } else if (inputs.operator === 'x'){
+//       result = Number(inputs.num1) * Number(inputs.num2); 
+//     } else if (inputs.operator === is it '%' or '/') {
+//       result = Number(inputs.num1) / Number(inputs.num2);
+// }
+
+// results.push(inputs);
+// }
+
+
+
+
+
+app.get('/maths', function(req, res){
+  console.log('...and the answer is:', results);
+  res.send(results);
+})
+
+app.post('/maths', function(req, res){
+  console.log( 'Here is some maths!!');
+  console.log('req.body is', req.body);
+  mathAboutIt(req.body);
+  res.sendStatus(201);
+})
+
+app.get('/results', function(req, res){
+  console.log('results logs are:', results);
+  res.send(results);
+})
+
 app.listen(PORT, () => {
-    console.log("listening on port", PORT);
-});
+  console.log ('Server is running on port', PORT)
+})
+
