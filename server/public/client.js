761adb1aa22e2ed6fb905309758d5fb0663c8e75
Dave Nash <davidjnash2@gmail.com>
2023-05-12 23:24:53 -0500

    added event listenr functins to onReady function in clientjs for calculator button clicks, added buttons in table on index, add background color in stylesheet

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,22 @@
-$('document.').ready(onReady);
+$('document').ready(onReady);
 
 function onReady(){
+    $('#zero').on('click', );
+    $('#one').on('click', );
+    $('#two').on('click', );
+    $('#three').on('click', );
+    $('#four').on('click', );
+    $('#five').on('click', );
+    $('#six').on('click', );
+    $('#seven').on('click', );
+    $('#eight').on('click', );
+    $('#nine').on('click', );
+    $('#plus').on('click', );
+    $('#minus').on('click', );
+    $('#multiply').on('click', );
+    $('#divide').on('click', );
+    $('#equals').on('click', );
+    $('#all-clear').on('click', );
 
 }
 
