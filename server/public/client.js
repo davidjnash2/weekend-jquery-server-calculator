5e4f2fc884609e60e199785d3b696b3b4ddb3d05
Dave Nash <davidjnash2@gmail.com>
2023-05-13 15:00:29 -0500

    on clientjs I added several-many event listeners for eventual stretch functionality and built out event handler functions sendMaths and getAnswers to link via Ajax to server side, as well as render function and clearFields function, still need to figure out how to isolate the value of the operator to send it over, but everything seems to be laid out correctly and working as far as I can tell so far, also updated html -again- to try to simplify base calculator layout, while keeping what I coded for stretch should I have time to make that run too, and in serverjs worked on doMaths function to handle the client-submitted equations, and added appropriate get and post functions for urls maths and answers

--- server/public/client.js
+++ server/public/client.js
@@ -1,23 +1,126 @@
-$('document').ready(onReady);
+$('document').ready(onReady); // jQuery command to run onReady function once page loads
 
-function onReady(){
-    $('#zero').on('click', );
-    $('#one').on('click', );
-    $('#two').on('click', );
-    $('#three').on('click', );
-    $('#four').on('click', );
-    $('#five').on('click', );
-    $('#six').on('click', );
-    $('#seven').on('click', );
-    $('#eight').on('click', );
-    $('#nine').on('click', );
-    $('#plus').on('click', );
-    $('#minus').on('click', );
-    $('#multiply').on('click', );
-    $('#divide').on('click', );
-    $('#equals').on('click', );
-    $('#all-clear').on('click', );
+function onReady(){ // function to run after page loads to set ready state functionality
+    console.log('jQuery in the hizzy!');
+
+
+    $('#answer-button').on('click', sendMaths()); // event listener for this button
+    $('#clear-inputs-button').on('click', clearFields()); // event listener for this button
+    // $('#sum').on('click', doAdd()); // event listener for this button
+    // $('#subtract').on('click', doSubtract()); // event listener for this button
+    // $('#multiply').on('click', doMultiply()); // event listener for this button
+    // $('#division').on('click', doDivide()); // event listener for this button
+
+
+
+    // $('#zero').on('click', ); // event listener for this button
+    // $('#one').on('click', ); // event listener for this button
+    // $('#two').on('click', ); // event listener for this button
+    // $('#three').on('click', ); // event listener for this button
+    // $('#four').on('click', ); // event listener for this button
+    // $('#five').on('click', ); // event listener for this button
+    // $('#six').on('click', ); // event listener for this button
+    // $('#seven').on('click', ); // event listener for this button
+    // $('#eight').on('click', ); // event listener for this button
+    // $('#nine').on('click', ); // event listener for this button
+    // $('#plus').on('click', ); // event listener for this button
+    // $('#minus').on('click', ); // event listener for this button
+    // $('#multiply').on('click', ); // event listener for this button
+    // $('#divide').on('click', ); // event listener for this button
+    // $('#equals').on('click', ); // event listener for this button
+    // $('#all-clear').on('click', ); // event listener for this button
+
+    getAnswers(); // run function here to grab all answer data as page loads
 
 }
 
 
+function sendMaths(event){ // event handler for answer button
+    // event.preventDefault(); // prevent default brower response
+    let numOne = $('#first-number').val();
+    let numTwo = $('#second-number').val();
+    //let operator = //????????? $(this).val();
+    console.log('The equation is:', numOne, '', 'operator', '', numTwo);
+
+    $.ajax({
+        method: 'POST',
+        url: '/maths',
+        data: {
+          numOne,
+          numTwo,
+          operator
+        }
+      }).then(function(response) { // check if response posts
+        console.log( 'Posted' );
+        getAnswers();
+      }).catch(function(error){
+        alert('Error with guess POST!');
+        console.log( 'Error with post:', error);
+      })
+    
+    
+} // end getAnswer function
+
+function getAnswers() {
+    $.ajax({
+      method: 'GET',
+      url: '/answers'
+    }).then(function (response){
+      console.log('The winning number is:', response);
+      renderToDom(response);
+    }).catch(function(error){
+      alert('request failed');
+      console.log('request failed', error);
+    });
+  }
+
+  function renderToDom(answers){
+    $('#interaction-log').empty();
+    console.log('The answers are in:', answers);
+    for (let answer of answers){
+      $('#interaction-log').append(`
+          <li>${answer.numOne} ${anwer.operator} ${answer.numTwo} = ${answer.result}</li>
+      `)
+    }
+  }
+
+// function doAdd(event) {
+//     // event.preventDefault();
+//     console.log('Add button clicked!');
+// } // end doAdd function
+
+// function doSubtract(event) {
+//     // event.preventDefault();
+//     console.log('Subtract button clicked!');
+// } // end doSubtract function
+
+// function doMultiply(event) {
+//     // event.preventDefault();
+//     console.log('Multiply button clicked!');
+// } // end doMultiply function
+
+// function doDivide(event) {
+//     // event.preventDefault();
+//     console.log('Divide button clicked!');
+// } // end doDivide function
+
+function clearFields(event){ // event handler function for C button
+    //event.preventDefault(); // prevent any default browswer response
+    $('#first-number').val(''); // clear fields
+    $('#second-number').val(''); // clear fields
+} // end clearFields function
+
+
+
+
+// to use later for stretch goal to delete selected line
+// function goAway() { // event handler for go away button
+//     console.log('go away button works!') // log to test
+//     $(this).parent().remove(); // remove each associated div section on click
+// } // end goAway function
+
+// let index = +$(this).closest('tr').children('#lineCounter').text();
+    // console.log(index);
+    // $(this).closest('tr').remove(); // assign removal to occur on clicked line
+    // employeesArray.splice(index, 1);
+    // console.log(employeesArray);
--- server/server.js
+++ server/server.js
@@ -6,42 +6,41 @@ const PORT = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
-let results = [];
+let answers = [];
 
 //1. { numOne: x, numTwo: x }
-// function mathAboutIt(inputs) {
-//  let result = 0;
-//     if (inputs.operator === '+'){
-//       result = Number(inputs.num1) + Number(inputs.num2);
-//     } else if (inputs.operator === '-'){
-//       result = Number(inputs.num1) - Number(inputs.num2);          
-//     } else if (inputs.operator === 'x'){
-//       result = Number(inputs.num1) * Number(inputs.num2); 
-//     } else if (inputs.operator === is it '%' or '/') {
-//       result = Number(inputs.num1) / Number(inputs.num2);
-// }
+function doMaths(inputs) {
+    if (inputs.operator === '+'){
+      inputs.result = Number(inputs.num1) + Number(inputs.num2);
+    } else if (inputs.operator === '-'){
+      inputs.result = Number(inputs.num1) - Number(inputs.num2);          
+    } else if (inputs.operator === 'x'){
+      inputs.result = Number(inputs.num1) * Number(inputs.num2); 
+    } else if (inputs.operator === '/') {
+      inputs.result = Number(inputs.num1) / Number(inputs.num2);
+}
 
-// results.push(inputs);
-// }
+answers.push(inputs);
+}
 
-
+console.log('Answers are:', answers)
 
 
 
 app.get('/maths', function(req, res){
-  console.log('...and the answer is:', results);
+  console.log('...and the answer is:', answers);
   res.send(results);
 })
 
 app.post('/maths', function(req, res){
   console.log( 'Here is some maths!!');
   console.log('req.body is', req.body);
-  mathAboutIt(req.body);
+  doMaths(req.body);
   res.sendStatus(201);
 })
 
-app.get('/results', function(req, res){
-  console.log('results logs are:', results);
+app.get('/answers', function(req, res){
+  console.log('results logs are:', answers);
   res.send(results);
 })
 
